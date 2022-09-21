7ece45c83f9e3d24cc977b0230367a1cdf7d5613
AlgebraManiacABC <dunnluke01@gmail.com>
2022-09-21 13:58:58 -0500

    Changed spaces to tabs

--- src/ansi.c
+++ src/ansi.c
@@ -2,20 +2,20 @@
 
 /**
  * Switches the terminal to the alternate buffer.
- * 
+ *
  */
 void alternateBuffer()
 {
-    printf("\x1b[?1049h");
+	printf("\x1b[?1049h");
 }
 
 /**
  * Switches the terminal back to the default buffer.
- * 
+ *
  * Clears the alternate buffer.
- * 
+ *
  */
 void restoreBuffer()
 {
-    printf("\x1b[?1049l");
+	printf("\x1b[?1049l");
 }
--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-    if(!isatty(STDOUT_FILENO))
-    {
-        fprintf(stderr,"Output is not to a terminal. Terminating.\n");
-        return -1;
-    }
-    if(!isatty(STDIN_FILENO))
-    {
-        fprintf(stderr,"Input is not from a terminal. Terminating.\n");
-        return -1;
-    }
-    alternateBuffer();
-    restoreBuffer();
+	if (!isatty(STDOUT_FILENO))
+	{
+		fprintf(stderr, "Output is not to a terminal. Terminating.\n");
+		return -1;
+	}
+	if (!isatty(STDIN_FILENO))
+	{
+		fprintf(stderr, "Input is not from a terminal. Terminating.\n");
+		return -1;
+	}
+	alternateBuffer();
+	restoreBuffer();
 }
